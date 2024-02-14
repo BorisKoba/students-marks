a17d929c638ab744ad326bfc350ff01dd1b277e1
BorisKoba <browi@mail.ru>
2024-02-14 15:30:12 +0300

    CW#71

--- src/main/java/telran/students/service/StudentsServiceImpl.java
+++ src/main/java/telran/students/service/StudentsServiceImpl.java
@@ -1,5 +1,8 @@
 package telran.students.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,10 +10,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.students.dto.Mark;
 import telran.students.dto.Student;
-import telran.students.exceptions.MarkIllegalStateException;
 import telran.students.exceptions.StudentIllegalStateException;
 import telran.students.exceptions.StudentNotFoundException;
 import telran.students.model.StudentDoc;
+import telran.students.repo.IdPhone;
 import telran.students.repo.StudentRepo;
 @Service
 @RequiredArgsConstructor
@@ -22,39 +25,121 @@ public class StudentsServiceImpl implements StudentsService {
 	public Student addStudent(Student student) {
 		long id = student.id();
 		if(studentRepo.existsById(id)) {
-			log.error("student with id{} already exsist", id);
+			log.error("student with id {} already exists", id);
 			throw new StudentIllegalStateException();
 		}
-		StudentDoc studentDoc = new StudentDoc(student); 
+		StudentDoc studentDoc = new StudentDoc(student);
 		studentRepo.save(studentDoc);
-		log.debug("student id{} saved", id);
+		log.debug("student {} has been saved", student);
 		return student;
 	}
 
 	@Override
-	@Transactional
 	public Mark addMark(long id, Mark mark) {
-		StudentDoc studentDoc = studentRepo.findById(id).orElseThrow(()-> new StudentNotFoundException());
-		log.debug("student with id{}", id);
-		if(studentDoc.getMarks().contains(mark)) {
-			log.error("student with id{}, mark {} already exsist", id, mark);
-			throw new MarkIllegalStateException();
-		}
-		studentDoc.getMarks().add(mark);
-		studentRepo.save(studentDoc);
-		log.debug("student id {} with mark {} saved", id, mark);
+		StudentDoc studentDoc = studentRepo.findById(id)
+				.orElseThrow(() -> new StudentNotFoundException());
+		List<Mark> marks = studentDoc.getMarks();
+		log.debug("student with id {}, has marks {} before adding new one",
+				id, marks);
+		marks.add(mark);
+		StudentDoc savedStudent = studentRepo.save(studentDoc);
+		log.debug("new marks after saving are {}", savedStudent.getMarks());
 		return mark;
 	}
 
 	@Override
 	@Transactional
 	public Student updatePhoneNumber(long id, String phoneNumber) {
-		StudentDoc studentDoc = studentRepo.findById(id).orElseThrow(()-> new StudentNotFoundException());
-		log.debug("student with id{}, old phone number{},new phone number{}", id, studentDoc.getPhone(),phoneNumber);
+		StudentDoc studentDoc = studentRepo.findById(id)
+				.orElseThrow(() -> new StudentNotFoundException());
+		log.debug("student with id {}, old phone number {}, new phone number {}",
+				id,studentDoc.getPhone(), phoneNumber);
 		studentDoc.setPhone(phoneNumber);
 		Student res = studentRepo.save(studentDoc).build();
-		log.debug("Student {}has been saved",res);
+		log.debug("Student {} has been saved ", res);
 		return res;
+	}
+
+	@Override
+	public Student removeStudent(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudent(long id) {
+		StudentDoc studentDoc = studentRepo.findStudentNoMarks(id);
+		if(studentDoc == null) {
+			throw new StudentNotFoundException();
+		}
+		log.debug("marks of found student {}", studentDoc.getMarks());	
+		Student student = studentDoc.build();
+		log.debug("found student {}", student);
+		return student;
+	}
+
+	@Override
+	public List<Mark> getMarks(long id) {
+		StudentDoc studentDoc = studentRepo.findStudentOnlyMarks(id);
+		if(studentDoc == null) {
+			throw new StudentNotFoundException();
+		}
+		List<Mark> res = studentDoc.getMarks();
+		log.debug("phone: {}, id: {}", studentDoc.getPhone(), studentDoc.getId());
+		log.debug("marks of found student {}", res);	
+		
+		return res;
+	}
+
+	@Override
+	public List<Student> getStudentsAllGoodMarks(int markThreshold) {
+		// Will be implemented on the CW #72
+		return null;
+	}
+
+	@Override
+	public List<Student> getStudentsFewMarks(int nMarks) {
+		// Will be implemented on the CW #72
+		return null;
+	}
+
+	@Override
+	public Student getStudentByPhoneNumber(String phoneNumber) {
+		IdPhone idPhone = studentRepo.findByPhone(phoneNumber);
+		
+		Student res = null;
+		if(idPhone != null) {
+			res = new Student(idPhone.getId(), idPhone.getPhone());
+		}
+		log.debug("student {}", res);
+		return res;
+	}
+
+	@Override
+	public List<Student> getStudentsByPhonePrefix(String prefix) {
+		List<IdPhone> idPhones = studentRepo.findByPhoneRegex(prefix + ".+");
+		List<Student> res = idPhones.stream()
+				.map(ip -> new Student(ip.getId(), ip.getPhone())).toList();
+		log.debug("students {}", res);
+		return res;
+	}
+
+	@Override
+	public List<Student> getStudentsMarksDate(LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getStudentsMarksMonthYear(int month, int year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> getStudentsGoodSubjectMark(int markThreshold) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/test/java/telran/students/StudentsMarksServiceTests.java
+++ src/test/java/telran/students/StudentsMarksServiceTests.java
@@ -1,58 +1,84 @@
 package telran.students;
 
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import static telran.students.TestDb.*;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import telran.students.dto.*;
 import telran.students.exceptions.*;
-import telran.students.model.StudentDoc;
 import telran.students.repo.StudentRepo;
 import telran.students.service.StudentsService;
 
 @SpringBootTest
 
 class StudentsMarksServiceTests {
-    @Autowired
-    StudentRepo studentRepo;
-    @Autowired
-    StudentsService studentService;
-    
-    Student student = new Student(123l, "0555555555");
-    Student student1 = new Student(124l, "0555555555");
-    Mark mark = new Mark("History", 5, LocalDate.now());
-    
-    @AfterEach
-    void clearRepo() {
-        studentRepo.deleteAll(); 
-    }
-    @Test 
-    void testAddStudent() {
-        assertEquals(student, studentService.addStudent(student));
-        assertThrowsExactly(StudentIllegalStateException.class,
-				()->studentService.addStudent(student));
-		StudentDoc studentDoc = studentRepo.findById(student.id()).orElse(null);
-		assertEquals(student, studentDoc.build());
-    }
-    @Test
-    void testUpdatePhoneNumber() {
-    	studentService.addStudent(student);
-    	Student studentUpdated = new Student(123l, "0566666666");
-		assertEquals(studentUpdated, studentService.updatePhoneNumber(123l, "0566666666"));
-		assertEquals("0566666666", studentRepo.findById(studentUpdated.id()).get().getPhone());
-		assertThrowsExactly(StudentNotFoundException.class,
-				() -> studentService.updatePhoneNumber(124l, "0566666666"));
+	@Autowired
+	StudentsService studentsService;
+	@Autowired
+	StudentRepo studentRepo;
+	@Autowired
+	TestDb testDb;
+	@BeforeEach
+	void setUp() {
+		testDb.createDb();
 	}
-    @Test
-    void testAddMark() {
-    	studentService.addStudent(student);
-    	assertEquals(mark, studentService.addMark(123l, mark));
-    	assertEquals(mark, studentRepo.findById(student.id()).get().getMarks().get(0));
-    	assertThrowsExactly(StudentNotFoundException.class,
-				() -> studentService.addMark(124l, mark));
-    	assertThrowsExactly(MarkIllegalStateException.class,
-				() -> studentService.addMark(123l, mark));
-    	
-    }
+
+	@Test
+	
+	void addStudentTest() {
+		//FIXME according to TestDb
+//		assertEquals(student, studentsService.addStudent(student));
+//		assertEquals(student, studentRepo.findById(ID1).orElseThrow().build());
+//		assertThrowsExactly(StudentIllegalStateException.class, ()->studentsService.addStudent(student));
+	}
+	@Test
+
+	void updatePhoneNumberTest() {
+		//FIXME according to TestDb
+//		assertEquals(studentUpdated, studentsService.updatePhoneNumber(ID1, PHONE2));
+//		assertEquals(studentUpdated, studentRepo.findById(ID1).orElseThrow().build());
+//		assertThrowsExactly(StudentNotFoundException.class,
+//				()->studentsService.updatePhoneNumber(ID1 + 1000, PHONE2));
+	}
+	@Test
+	
+	void addMarkTest() {
+		//FIXME according to TestDb
+//		assertFalse(studentRepo.findById(ID1).orElseThrow().getMarks().contains(mark));
+//		assertEquals(mark, studentsService.addMark(ID1, mark));
+//		assertTrue(studentRepo.findById(ID1).orElseThrow().getMarks().contains(mark));
+//		assertThrowsExactly(StudentNotFoundException.class,
+//				()->studentsService.addMark(ID1 + 1000, mark));
+		
+	}
+	@Test
+	void getStudentTest() {
+		assertEquals(students[0], studentsService.getStudent(ID1));
+		assertThrowsExactly(StudentNotFoundException.class, ()->studentsService.getStudent(100000));
+	}
+	@Test
+	void getMarksTest() {
+		assertArrayEquals(marks[0], studentsService.getMarks(ID1).toArray(Mark[]::new));
+		assertThrowsExactly(StudentNotFoundException.class, ()->studentsService.getMarks(100000));
+	}
+	@Test
+	void getStudentByPhoneNumberTest() {
+		assertEquals(students[0], studentsService.getStudentByPhoneNumber(PHONE1));
+	}
+	@Test
+	void getStudentsByPhonePrefix() {
+		List<Student> expected = List.of(students[0], students[6]);
+		assertIterableEquals(expected, studentsService.getStudentsByPhonePrefix("051"));
+	}
+	//TODO tests of the interface methods for HW #71 (see StudentsService interface)
+	
+	
+
 }
